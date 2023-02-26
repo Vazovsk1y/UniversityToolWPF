27e96d4e94a0fc54423bc2df56b18a0b87ad7b32
Vazovsk1y <anolrey.b@gmail.com>
2023-02-26 17:52:23 +0300

    MessageBus service added.

---
+++ UniversityTools/Services/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UniversityTool.Services
+{
+    public interface IMessageBus
+    {
+        IDisposable RegisterHandler<T>(Action<T> handler);
+        void Send<T>(T message);
+    }
+}
---
+++ UniversityTools/Services/Implementaions/MessageBusService.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.Linq;
+
+namespace UniversityTool.Services.Implementaions
+{
+    internal class MessageBusService
+    {
+        private readonly Dictionary<Type, IEnumerable<WeakReference>> _subscriptions = new();
+        private readonly ReaderWriterLockSlim _locker = new();
+        private class Subscription<T> : IDisposable
+        {
+            private readonly WeakReference<MessageBusService> _bus;
+            public Action<T> Handler { get; }
+
+            public Subscription(MessageBusService bus, Action<T> handler)
+            {
+                _bus = new(bus);
+                Handler = handler;
+            }
+
+            public void Dispose()
+            {
+                if (!_bus.TryGetTarget(out var bus))
+                    return;
+
+                var Lock = bus._locker;
+                Lock.EnterWriteLock();
+                var messageType = typeof(T);
+                try
+                {
+                    if (!bus._subscriptions.TryGetValue(messageType, out var refs))
+                        return;
+
+                    var updatedRefs = refs.Where(r => r.IsAlive).ToList();
+
+                    WeakReference? currentReference = null;
+                    foreach (var item in updatedRefs)
+                    {
+                        if (ReferenceEquals(item.Target, this))
+                        {
+                            currentReference = item;
+                            break;
+                        }
+                    }
+                    if (currentReference == null)
+                        return;
+
+                    updatedRefs.Remove(currentReference);
+                    bus._subscriptions[messageType] = updatedRefs;
+
+                }
+                finally
+                {
+                    Lock.ExitWriteLock();
+                }
+            }
+        }
+
+        public IDisposable RegisterHandler<T>(Action<T> handler)
+        {
+            var subscription = new Subscription<T>(this, handler);
+
+            _locker.EnterWriteLock();
+            try
+            {
+                var weakRef = new WeakReference(subscription);
+                var messageType = typeof(T);
+
+                _subscriptions[messageType] = _subscriptions.TryGetValue(messageType, out var subscriptions) 
+                    ? subscriptions.Append(weakRef) : new[] { weakRef };
+            }
+            finally
+            {
+                _locker.ExitWriteLock();
+            }
+
+            return subscription;
+        }
+
+        public void Send<T>(T message)
+        {
+            if (GetHandlers<T>() is not { } handlers)
+                return;
+
+            foreach (var item in handlers)
+            {
+                item(message);
+            }
+        }
+
+        private IEnumerable<Action<T>>? GetHandlers<T>()
+        {
+            var handlers = new List<Action<T>>();
+            var messageType = typeof(T);
+            bool isDiedRefsExist = false;
+
+            _locker.EnterReadLock();
+            try
+            {
+                if (!_subscriptions.TryGetValue(messageType, out var refs))
+                    return null;
+
+                foreach (var item in refs)
+                {
+                    if (item.Target is Subscription<T> { Handler: var handler })
+                        handlers.Add(handler);
+                    else
+                        isDiedRefsExist = true;
+                }
+            }
+            finally
+            {
+                _locker.ExitReadLock();
+            }
+
+            if (!isDiedRefsExist) return handlers;
+
+            // clean up the died references
+            _locker.EnterWriteLock();
+            try
+            {
+                if (!_subscriptions.TryGetValue(messageType, out var refs))
+                    if (refs.Where(r => r.IsAlive).ToArray() is { Length: > 0 } newRefs)
+                        _subscriptions[messageType] = newRefs;
+                    else
+                        _subscriptions.Remove(messageType);
+            }
+            finally
+            {
+                _locker.ExitWriteLock();
+            }
+
+            return handlers;
+        }
+    }
+}
