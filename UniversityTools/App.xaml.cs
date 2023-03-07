89fe6b430e6eccf80eb09569a8f7cb0500d2c6fc
Vazovsk1y <anolrey.b@gmail.com>
2023-03-07 22:05:11 +0300

    Create a one Instance app handler.

--- UniversityTools/App.xaml.cs
+++ UniversityTools/App.xaml.cs
@@ -19,6 +19,8 @@ namespace UniversityTool
     /// </summary>
     public partial class App : Application
     {
+        private static readonly string UniqueEventName = "UniversityTool";
+
         private static IServiceProvider? _services;
 
         public static IServiceProvider Services => _services ??= InitializeServices().BuildServiceProvider();
@@ -82,8 +84,29 @@ namespace UniversityTool
             return services;
         }
 
+        #region --#2 Way App Start--
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            bool isNewInstance = false;
+            try
+            {
+                EventWaitHandle eventWaitHandle = EventWaitHandle.OpenExisting(UniqueEventName); // here will be exception if app is not starting
+                eventWaitHandle.Set();
+                Shutdown();
+                return;
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                isNewInstance = true;
+            }
+
+            if (isNewInstance)
+            {
+                EventWaitHandle eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+                Current.Exit += (sender, args) => eventWaitHandle.Close();
+            }
+
             base.OnStartup(e);
             Services.GetRequiredService<IMainWindowService>().OpenWindow();
         }
@@ -92,6 +115,48 @@ namespace UniversityTool
         {
             base.OnExit(e);
             Services.GetRequiredService<IMainWindowService>().CloseWindow();
+            Current.Shutdown();
         }
+
+        #endregion
+
+        #region --#1 Way App Start--
+
+        #region --Start and Exit--
+
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    if (!InstanceCheck())
+        //    {
+        //        MessageBox.Show("IsAlreadyStarted!");
+        //        Current.Shutdown();
+        //        return;
+        //    }
+        //    Services.GetRequiredService<IMainWindowService>().OpenWindow();
+        //}
+
+        //protected override void OnExit(ExitEventArgs e)
+        //{
+        //    base.OnExit(e);
+        //    Services.GetRequiredService<IMainWindowService>().CloseWindow();
+        //    Current.Shutdown();
+        //}
+
+        #endregion
+
+        //private static Mutex? InstanceCheckMutex;
+        //private static bool InstanceCheck()
+        //{
+        //    var mutex = new Mutex(true, "UniversityTool", out bool isNew);
+        //    if (isNew)
+        //        InstanceCheckMutex = mutex;
+        //    else
+        //        mutex.Dispose();
+
+        //    return isNew;
+        //}
+
+        #endregion
     }
 }
