37ba4ffd2b76c4bf94966691ece0050173e4cc9f
Vazovsk1y <anolrey.b@gmail.com>
2023-03-22 18:50:30 +0300

    -add new base class for dialog viem models. -add student vm, window.

---
+++ UniversityTools/ViewModels/Base/DialogViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using UniversityTool.Domain.Messages.Base;
+using UniversityTool.Domain.Services.Base;
+using UniversityTool.Domain.Services.DataServices.Base;
+using UniversityTool.Infastructure.Commands;
+
+namespace UniversityTool.ViewModels.Base
+{
+    internal abstract class DialogViewModel<T> : TitledViewModel where T : IBaseWindowService
+    {
+        #region --Fields--
+
+        protected readonly IMessageBusService _messageBusService;
+        protected readonly T _windowService;
+
+        #endregion
+
+        #region --Properties--
+
+
+
+        #endregion
+
+        #region --Constructors--
+
+        public DialogViewModel() { }
+
+        public DialogViewModel(IMessageBusService messageBus, T windowService) 
+        {
+            AcceptCommand = new RelayCommand(OnAccepting, OnCanAccept);
+            CancelCommand = new RelayCommand(OnCanceling, OnCanCancel);
+            _messageBusService = messageBus;
+            _windowService = windowService;
+        }
+
+        #endregion
+
+        #region --Commands--
+
+        public ICommand CancelCommand { get; private set; }
+        public ICommand AcceptCommand { get; private set; }
+
+        protected virtual bool OnCanCancel(object p) => true;
+
+        protected virtual void OnCanceling(object p) => _windowService.CloseWindow();
+
+        protected virtual bool OnCanAccept(object p) => true;
+
+        protected abstract void OnAccepting(object action);
+
+        #endregion
+
+        #region --Methods--
+
+        protected virtual async Task SendMessageAsync<T>(T message) where T : BaseMessage =>
+          await Task.Run(() => _messageBusService.Send(message)).ConfigureAwait(false);
+
+        #endregion
+    }
+}
---
+++ UniversityTools/ViewModels/StudentAddViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UniversityTool.Domain.Models;
+using UniversityTool.Domain.Services.DataServices.Base;
+using UniversityTool.ViewModels.Base;
+using System;
+
+namespace UniversityTool.ViewModels
+{
+    internal class StudentAddViewModel : TitledViewModel
+    {
+        #region --Fields--
+
+        private IEnumerable<Group> _groups;
+        private Group _selectedGroup;
+        private string _studentName;
+        private string _studentSurname;
+        private string _studentThirdName;
+
+        #endregion
+
+        #region --Properties--
+
+        public string StudentName
+        {
+            get => _studentName;
+            set => Set(ref _studentName, value);
+        }
+
+        public string StudentSurname
+        {
+            get => _studentSurname;
+            set => Set(ref _studentSurname, value);
+        }
+
+        public string StudentThirdName
+        {
+            get => _studentThirdName;
+            set => Set(ref _studentThirdName, value);
+        }
+
+        public IEnumerable<Group> Groups 
+        { 
+            get => _groups; 
+            set => Set(ref _groups, value); 
+        }
+
+        public Group SelectedGroup 
+        { 
+            get => _selectedGroup; 
+            set => Set(ref _selectedGroup, value); 
+        }
+
+        #endregion
+
+        #region --Constructors--
+
+        public StudentAddViewModel() 
+        {
+            if (!App.IsDesignMode)
+                throw new InvalidOperationException("Constructor only for designTime");
+            WindowTitle = "Student Add";
+        }
+
+        #endregion
+
+        #region --Commands--
+
+
+
+        #endregion
+
+        #region --Methods--
+
+
+
+        #endregion
+    }
+}
