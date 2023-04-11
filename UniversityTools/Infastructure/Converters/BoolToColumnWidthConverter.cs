116ea17d467df4a1b1b1c4ede420e116753c2cc6
Vazovsk1y <anolrey.b@gmail.com>
2023-04-11 20:17:58 +0300

    -add workSpace stretch behaivour.

---
+++ UniversityTools/Infastructure/Converters/BoolToColumnWidthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UniversityTool.Infastructure.Converters
+{
+    internal class BoolToColumnWidthConverter : IValueConverter
+    {
+        private const string COLUMN_WIDTH_IF_TRUE = "0";
+        private const string COLUMN_WIDTH_IF_FALSE = "2*";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? COLUMN_WIDTH_IF_TRUE : COLUMN_WIDTH_IF_FALSE;
+            }
+            return COLUMN_WIDTH_IF_FALSE;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
