67c6d1897ea9c47078e2b23681203c8372b20f9e
Vazovsk1y <anolrey.b@gmail.com>
2023-04-16 21:41:17 +0300

    -modifiy benchmark app.

--- BenchmarksApp/Program.cs
+++ BenchmarksApp/Program.cs
@@ -1,45 +1,13 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using Microsoft.EntityFrameworkCore;
+﻿using BenchmarkDotNet.Running;
 using Microsoft.Extensions.DependencyInjection;
-using UniversityTool.DataBase.Context;
 using UniversityTool.DataBase.Registrators.Repositories;
 using UniversityTool.DataBase.Registrators.Services;
-using UniversityTool.DataBase.Repositories;
-using UniversityTool.Domain.Codes;
-using UniversityTool.Domain.Repositories;
-using UniversityTool.Domain.Services.DataServices;
 
 namespace BenchmarksApp
 {
     public class Test
     {
-        private readonly IDepartamentTreeService _service;
-
-        public Test()
-        {
-            _service = Program.Service.GetRequiredService<IDepartamentTreeService>();  
-        }
-
-        [Benchmark(Baseline =true)]
-        public async Task InitializeFullTreeAsync() => await Task.Run(async () =>
-        {
-            var response = await _service.GetFullDepartamentsTree().ConfigureAwait(false);
-            if (response.StatusCode == OperationResultStatusCode.Success)
-            {
-                Task.Run(() => { var data = response.Data; });
-            }
-        });
-
-        [Benchmark]
-        public async Task InitializeSecond()
-        {
-            var response = await _service.GetFullDepartamentsTree().ConfigureAwait(false);
-            if (response.StatusCode == OperationResultStatusCode.Success)
-            {
-                Task.Run(() => { var data = response.Data; });
-            }
-        }
+        
     }
 
     internal class Program
@@ -52,10 +20,7 @@ namespace BenchmarksApp
             .AddDbServices()
             .AddDbRepositories()
             .AddTransient<Test>()
-            .AddDbContextFactory<UniversityToolDbContext>(o =>
-            {
-                o.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=UniversityToolDB;Trusted_Connection=True;");
-            });
+            ;
 
         public static void Main(string[] args)
         {
