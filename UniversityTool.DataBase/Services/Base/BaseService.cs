67143c90b23515f9ad70c864c04661413f8e3175
Vazovsk1y <anolrey.b@gmail.com>
2023-04-17 20:04:41 +0300

    -add logger to base service.

--- UniversityTool.DataBase/Services/Base/BaseService.cs
+++ UniversityTool.DataBase/Services/Base/BaseService.cs
@@ -6,18 +6,23 @@ using UniversityTool.Domain.Repositories.Base;
 using UniversityTool.Domain.Services.DataServices.Base;
 using UniversityTool.DataBase.Properties;
 using UniversityTool.Domain.Responses;
+using Microsoft.Extensions.Logging;
 
 namespace UniversityTool.DataBase.Services.Base
 {
     internal abstract class BaseService<T> : IBaseService<T> where T : BaseModel
     {
+        protected readonly ILogger<BaseService<T>> _logger;
         protected readonly IBaseRepository<T> _repository;
         protected readonly IResponseFactory<T> _responseFactory;
         protected T Item { get; set; }
         protected IEnumerable<T> Items { get; set; }
 
-        public BaseService(IBaseRepository<T> repository, IResponseFactory<T> responceFactory)
+        public BaseService(IBaseRepository<T> repository, 
+            IResponseFactory<T> responceFactory, 
+            ILogger<BaseService<T>> logger)
         {
+            _logger = logger;
             _repository = repository;
             _responseFactory = responceFactory;
         }
@@ -31,24 +36,27 @@ namespace UniversityTool.DataBase.Services.Base
         {
             try
             {
+               _logger.LogInformation($"Adding {entityToAdd.GetType()} is started.");
                Item = await _repository.Add(entityToAdd).ConfigureAwait(false);
 
                 if (Item is null)
                 {
+                    _logger.LogInformation($"Entity is not exist in database");
                     return _responseFactory.CreateResponce(Resources.AddingErrorMessage, OperationResultStatusCode.Fail, Item);
                 }
             }
             catch(DbUpdateException ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.NullParametrErrorMessage + Resources.AddingErrorMessage, OperationResultStatusCode.Fail, Item);
             }
             catch (Exception ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.AddingErrorMessage, OperationResultStatusCode.Fail, Item);
             }
 
+            _logger.LogInformation($"Operation succsesfully completed.");
             return _responseFactory.CreateResponce(Resources.AddingSuccessMessage, OperationResultStatusCode.Success, Item);
         }
 
@@ -56,24 +64,27 @@ namespace UniversityTool.DataBase.Services.Base
         {
             try
             {
+                _logger.LogInformation($"Getting all entities is started.");
                 Items = await _repository.GetAll().ConfigureAwait(false);
 
                 if (Items is null)
                 {
+                    _logger.LogInformation($"Entities was null");
                     return _responseFactory.CreateResponce(string.Empty, OperationResultStatusCode.Fail, Items);
                 }
             }
             catch (DbUpdateException ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(string.Empty, OperationResultStatusCode.Fail, Items);
             }
             catch (Exception ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(string.Empty, OperationResultStatusCode.Fail, Items);
             }
 
+            _logger.LogInformation($"Operation succsesfully completed.");
             return _responseFactory.CreateResponce(Resources.AddingSuccessMessage, OperationResultStatusCode.Success, Items);
         }
 
@@ -81,24 +92,27 @@ namespace UniversityTool.DataBase.Services.Base
         {
             try
             {
+                _logger.LogInformation($"Updating {entity.GetType()} is started");
                 Item = await _repository.Update(entity).ConfigureAwait(false);
 
                 if (Item is null)
                 {
+                    _logger.LogInformation($"Entity is not exist in database");
                     return _responseFactory.CreateResponce(Resources.UpdateErrorMessage, OperationResultStatusCode.Fail, Item);
                 }
             }
             catch(DbUpdateException ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.NullParametrErrorMessage + Resources.UpdateErrorMessage, OperationResultStatusCode.Fail, Item);
             }
             catch(Exception ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.UpdateErrorMessage, OperationResultStatusCode.Fail, Item);
             }
 
+            _logger.LogInformation($"Operation succsesfully completed.");
             return _responseFactory.CreateResponce(Resources.UpdateSuccessMessage, OperationResultStatusCode.Success, Item);
         }
 
@@ -106,24 +120,27 @@ namespace UniversityTool.DataBase.Services.Base
         {
             try
             {
+                _logger.LogInformation($"Deleting {entity.GetType()} is started");
                 Item = await _repository.Delete(entity.Id).ConfigureAwait(false);
 
                 if (Item is null)
                 {
+                    _logger.LogInformation($"Entity is not exist in database");
                     return _responseFactory.CreateResponce(Resources.DeleteErrorMessage, OperationResultStatusCode.Fail, Item);
                 }
             }
             catch (DbUpdateException ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.NullParametrErrorMessage + Resources.DeleteErrorMessage, OperationResultStatusCode.Fail, Item);
             }
             catch (Exception ex)
             {
-                Debug.WriteLine(ex);
+                _logger.LogError($"Exception thrown {ex.Message}");
                 return _responseFactory.CreateResponce(Resources.DeleteErrorMessage, OperationResultStatusCode.Fail, Item);
             }
 
+            _logger.LogInformation($"Operation succsesfully completed.");
             return _responseFactory.CreateResponce(Resources.DeleteSuccessMessage, OperationResultStatusCode.Success, Item);
         }
     }
--- UniversityTool.DataBase/Services/GroupService.cs
+++ UniversityTool.DataBase/Services/GroupService.cs
@@ -1,4 +1,5 @@
-﻿using UniversityTool.DataBase.Services.Base;
+﻿using Microsoft.Extensions.Logging;
+using UniversityTool.DataBase.Services.Base;
 using UniversityTool.Domain.Models;
 using UniversityTool.Domain.Repositories.Base;
 using UniversityTool.Domain.Responses;
@@ -8,7 +9,8 @@ namespace UniversityTool.DataBase.Services
 {
     internal class GroupService : BaseService<Group>, IGroupService
     {
-        public GroupService(IBaseRepository<Group> repository, IResponseFactory<Group> responceFactory) : base(repository, responceFactory)
+        public GroupService(IBaseRepository<Group> repository, IResponseFactory<Group> responceFactory, ILogger<GroupService> logger) 
+            : base(repository, responceFactory, logger)
         {
         }
     }
