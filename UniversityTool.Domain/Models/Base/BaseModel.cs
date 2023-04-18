d9630c1f620280900fcc27c9fe02cbb08a491632
Vazovsk1y <anolrey.b@gmail.com>
2023-04-18 17:33:24 +0300

    -add new "DateUpdated" property to base model.

--- UniversityTool.Domain/Models/Base/BaseModel.cs
+++ UniversityTool.Domain/Models/Base/BaseModel.cs
@@ -3,6 +3,9 @@
     public abstract class BaseModel
     {
         public int Id { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        public DateTime DateUpdated { get; set; }
     }
 }
