625b8b5c01d3846e7c927b44399a1b5d51a03dd1
Vazovsk1y <anolrey.b@gmail.com>
2023-04-26 20:12:54 +0300

    -remove unused try/catch in repositories.

--- UniversityTool.DataBase/Repositories/DepartamentTreeRepository.cs
+++ UniversityTool.DataBase/Repositories/DepartamentTreeRepository.cs
@@ -9,28 +9,21 @@ namespace UniversityTool.DataBase.Repositories
     {
         protected readonly IDbContextFactory<UniversityToolDbContext> _contextFactory;
 
-        public DepartamentTreeRepository(IDbContextFactory<UniversityToolDbContext>  contextFactory)
+        public DepartamentTreeRepository(IDbContextFactory<UniversityToolDbContext> contextFactory)
         {
             _contextFactory = contextFactory;
         }
 
         public async Task<IEnumerable<Departament>> GetDepartamentsRelations()
         {
-            try
-            {
-                using UniversityToolDbContext context = _contextFactory.CreateDbContext();
-                IEnumerable<Departament> departaments =
-                    await context.Departaments
-                    .Include(d => d.Groups)
-                    .ThenInclude(g => g.Students)
-                    .ToListAsync().ConfigureAwait(false);
+            using UniversityToolDbContext context = _contextFactory.CreateDbContext();
+            IEnumerable<Departament> departaments =
+                await context.Departaments
+                .Include(d => d.Groups)
+                .ThenInclude(g => g.Students)
+                .ToListAsync().ConfigureAwait(false);
 
-                return departaments;
-            }
-            catch
-            {
-                throw;
-            }
+            return departaments;
         }
     }
 }
